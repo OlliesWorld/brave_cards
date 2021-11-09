404054dde4a194d3c1cad7cf23088eeebf74225c
Roni <rtlockwoodwork@gmail.com>
2021-11-08 20:24:09 -0700

    added flip

---
+++ bravecards-next/pages/cardFlip.js
@@ -0,0 +1,17 @@
+const cardFlip = ({cards, src}) => {
+return (
+    <>
+<div className="card">
+   <div className="front">
+     <p>front side</p>
+   </div>
+   
+   <div className="back">
+     <p>back side</p>
+   </div>
+</div>
+
+    </>
+)
+}
+export default cardFlip
--- bravecards-next/pages/index.js
+++ bravecards-next/pages/index.js
@@ -8,34 +8,45 @@ const Home = ({cards, src}) => {
   return (
     <> 
     {cards && (
-      <div className="main ">
+      <div className="main bg-purple-700 pt-4">
         <div className="header my-4 text-center">
-          <h1 className="text-4xl">These Women inspire me to be BRAVE!</h1>
+          <h1 className="text-6xl">These Women inspire me to be (creator word)</h1>
         </div>
         
         <div className="grid md:grid-cols-2 lg:grid-cols-4 justify-items-center">
           {cards.map((card) => (
             <Link key={card._id} href={`card/${card.slug.current}`} passHref target="_blank">
-              <div  className="max-w-md rounded overflow-hidden shadow-lg mb-4 bg-white">
+              <div  className="card max-w-md rounded overflow-hidden shadow-lg mb-4 bg-white cursor-pointer">
+                <div className="front">
                 <div >
                 <Image identifier="main-image" image={card.mainImage} alt="This is a Rebel Girl"/>
                 
-                <div className="relative w-12 h-12 bg-purple-100 rounded-full flex justify-center items-center text-center p-5 shadow-xl -mt-16 ml-2">
+                <div className="relative w-12 h-12 bg-yellow-400 rounded-full flex justify-center items-center text-center p-5 shadow-xl -mt-16 ml-2">
                   
                   {card.type}
                 </div>
                 </div >
-              <div className="px-6 py-2 mt-4 bg-blue-400">
+              <div className="px-6 py-2 mt-4 bg-blue-700">
                 <div className="font-bold text-2xl mb-2 text-center">{card.alias}</div>
                 <div className="flex justify-center space-x-4">
                   <div>{card.power}</div>
                   <div>{card.reign}</div>
                 </div>
               </div>
-             
+              </div>
+              <div  className="back max-w-md rounded overflow-hidden shadow-lg mb-4 bg-white">
+              <div className="px-6  mt-4 bg-blue-400">
+                <div className="font-bold text-2xl mb-2 text-center">{card.name}</div>
+                <div className="">
+                  <div>Power: {card.power}</div>
+                  <div>Reign: {card.reign}</div>
+                </div>
+              </div>
+                  <div className="p-4"><span className="font-bold">My Why:</span> {card.theWhy}</div>
+                  </div>
           </div>
             </Link>
-
+                
           ))}
         </div>
         </div>
