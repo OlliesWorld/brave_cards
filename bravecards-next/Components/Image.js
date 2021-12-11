99e2017e4ff365aba30cdfa87c47b37d3a4f9449
Roni <rtlockwoodwork@gmail.com>
2021-12-10 17:24:47 -0700

    image size

--- bravecards-next/Components/Image.js
+++ bravecards-next/Components/Image.js
@@ -4,7 +4,7 @@ import { urlFor } from "../sanity"
 const Image = ({identifier, image}) => {
     return (
         <div className={identifier === "main-image" ? "main-image" : "image"}>
-            <img src={urlFor(image)} height={100} width={300} className="h-72" />
+            <img src={urlFor(image)} className="h-60 w-full" />
         </div>
     )
 }
