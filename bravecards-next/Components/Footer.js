932ab69c9b13dddaf0eea4e78fc2edd86bb21eb4
Roni <rtlockwoodwork@gmail.com>
2023-03-07 15:36:15 -0700

    grid on nav links

--- bravecards-next/Components/Footer.js
+++ bravecards-next/Components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="bg-blue-900 text-center py-2 text-blue-50">
             <h2>🙏 for visiting!</h2>
              <p>Created with 💙 by <a href="https://roni.rocks/"  rel="noreferrer"  className="text-green-800 text-xl font-bold">Roni</a> </p>
-             <a href="https://brave.sanity.studio/">Login</a>
+             {/* <a href="https://brave.sanity.studio/">Login</a> */}
             <p>&copy; {new Date().getFullYear()}  Brave_Cards</p>
         </div>
     )
